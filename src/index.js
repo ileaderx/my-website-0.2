9b4b83ba0ff0e1215edafc4678fa697a9c707732
ileaderx <i.leaderx@hotmail.com>
2023-03-14 17:05:56 +0300

    fixed redirects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, BrowserRouter as Routes, Route } from 'react-router-dom';
+import { BrowserRouter as  Routes, Route, BrowserRouter, Router } from 'react-router-dom';
 import App from './App';
 import Links  from './Links';
 
