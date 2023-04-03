8d2905deff02d3c72d3c5e7dd558633244bbc724
ileaderx <i.leaderx@hotmail.com>
2023-04-03 23:58:33 +0800

    finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as  Routes, Route, BrowserRouter, Router } from 'react-router-dom';
+import { BrowserRouter as  Routes, Route, BrowserRouter } from 'react-router-dom';
 import App from './App';
 import Links  from './Links';
 
